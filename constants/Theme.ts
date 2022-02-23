c1f83cbe950069af7e364e4f6954643054faa642
cochumo <stream99990@gmail.com>
2022-02-24 01:11:37 +0900

    update: as constにしました

--- constants/Theme.ts
+++ constants/Theme.ts
@@ -1,7 +1,14 @@
+declare module "styled-components" {
+  export interface DefaultTheme {
+    background: string;
+    text: string;
+  }
+}
+
 /**
  * @remarks https://twitter.com/pulpxstyle/status/1494279960437358594/photo/3
  */
 export const defaultTheme = {
   text: "#4a5e28",
   background: "#839c5d",
-};
+} as const;
